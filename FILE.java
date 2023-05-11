8fd039231b456ffe3e46b10e1e0561b4ec1ad848
aasis-io <heyashishthapa@gmail.com>
2023-05-11 11:32:03 +0545

    Java Class Begins

---
+++ FILE.java
@@ -0,0 +1,9 @@
+public class FILE{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        int a=10;
+        int b=10;
+        int c= a+b;
+        System.out.println("result: " + c);
+    }
+}
